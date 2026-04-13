6ef3f5a708e966c7982f2be90da280e27c722b22
Sabrina Aska <askach44@gmail.com>
2026-04-13 11:57:18 +0700

    fix: [AS] clean code for routes

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -10,7 +10,9 @@ router.post("/auth/register-company", AuthController.registerCompany);
 router.post("/auth/login", AuthController.login);
 router.post("/auth/refresh", AuthController.refreshToken);
 router.post("/auth/logout", AuthController.logout);
-router.get("/auth/profile", authMiddleware, AuthController.profile);
+
+// Profile
+router.get("/profile", authMiddleware, AuthController.profile);
 
 // Example protected route
 // router.get(
